f407f0fdfd1f4d60d634221d79d21909f030c9c1
Khizer04 <122547501+Khizer04@users.noreply.github.com>
2024-09-29 19:44:59 +0500

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+function Hero() {
+  return (
+    <div className="hero">
+      <img
+        className="hero--grid"
+        src="../../src/images/photo-grid.png"
+        alt="photo grid."
+      />
+      <h1 className="hero--heading">Online Experiences</h1>
+      <p className="hero--paragraph">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <img
+          src="../../src/images/airbnb-logo.png"
+          alt="airbnb logo."
+          className="nav--logo"
+        />
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
